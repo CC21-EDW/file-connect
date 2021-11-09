7e7b899848815bbf9990b26fabd5e21b8d4c079e
ane <arthur.neudeck@giniality.com>
2021-11-09 06:25:04 +0100

    #1: first successful read (without extension)

--- src/test/java/com/baloise/open/edw/fileconnect/infrastructure/file/model/ActivityTest.java
+++ src/test/java/com/baloise/open/edw/fileconnect/infrastructure/file/model/ActivityTest.java
@@ -1,32 +1,45 @@
 package com.baloise.open.edw.fileconnect.infrastructure.file.model;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 public class ActivityTest {
 
     @Test
     public void givenFile_whenRead_thenConvertedDto() throws IOException {
-        Path xmlFile = new File("src/test/resources/test-data/Cycle-20150105-1200-00000.tcx").toPath();
-        XmlMapper xmlMapper = new XmlMapper();
+        File xmlFile = new File("src/test/resources/test-data/Run-20151103-1813-07944.tcx");
+
+        JavaTimeModule timeModule = new JavaTimeModule();
+        timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")));
+
+        JacksonXmlModule module = new JacksonXmlModule();
+        module.setDefaultUseWrapper(false);
+        XmlMapper xmlMapper = new XmlMapper(module);
+
         xmlMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        String xmlContent = new String(Files.readAllBytes(xmlFile));
-        TrainingCenterDatabaseT db = xmlMapper.readValue(xmlContent, TrainingCenterDatabaseT.class);
+        xmlMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+        xmlMapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+        xmlMapper.registerModule(timeModule);
+
+        xmlMapper.registerModule(new JaxbAnnotationModule());
+        xmlMapper.registerModule(new Jdk8Module());
+
+        TrainingCenterDatabaseT db = xmlMapper.readValue(new FileReader(xmlFile), TrainingCenterDatabaseT.class);
         Assertions.assertNotNull(db);
         Assertions.assertNotNull(db.activities);
     }
